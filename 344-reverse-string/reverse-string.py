b221b7228dba3804dc6d86da734135879b4b4931
deleena22 <146045276+deleena22@users.noreply.github.com>
2026-06-08 20:58:30 -0700

    Time: 3 ms (55.65%) | Memory: 19.8 MB (43.46%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+
+        p1, p2 = 0, len(s) - 1
+
+        while p1 < p2:
+            s[p1], s[p2] = s[p2], s[p1]
+            p1 += 1
+            p2 -= 1
+        
+        return s
+        
